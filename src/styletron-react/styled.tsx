98993808e74e29d0b12627f761cc488037127dd8
Tian Pan <puncsky@gmail.com>
2020-08-25 21:13:25 -0700

    add more styled component theme support (#109)

---
+++ src/styletron-react/styled.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import {
+  createStyled,
+  StyletronComponent,
+  StyletronWrapper,
+} from "styletron-react";
+import { driver, getInitialStyle, StyleObject } from "styletron-standard";
+import { Consumer } from "./theme-provider";
+import { Theme } from "./theme-types";
+
+const wrapper: StyletronWrapper = (StyledComponent) =>
+  function withThemeHOC(props) {
+    return (
+      <Consumer>
+        {(theme) => <StyledComponent {...props} theme={theme} />}
+      </Consumer>
+    );
+  };
+
+export interface StyledFn {
+  <
+    C extends keyof JSX.IntrinsicElements | React.ComponentType<any>,
+    P extends object
+  >(
+    component: C,
+    style: (props: P & { theme: Theme }) => StyleObject
+  ): StyletronComponent<
+    Pick<
+      React.ComponentProps<C>,
+      Exclude<keyof React.ComponentProps<C>, { className: string }>
+    > &
+      P
+  >;
+
+  <C extends keyof JSX.IntrinsicElements | React.ComponentType<any>>(
+    component: C,
+    style: StyleObject
+  ): StyletronComponent<
+    Pick<
+      React.ComponentProps<C>,
+      Exclude<keyof React.ComponentProps<C>, { className: string }>
+    >
+  >;
+}
+
+export const styled: StyledFn = createStyled({
+  wrapper,
+  getInitialStyle,
+  driver,
+});
