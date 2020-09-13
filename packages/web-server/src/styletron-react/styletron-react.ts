682b7c78e33ab14b282bc0a5e7d067915047a73e
Tian Pan <puncsky@gmail.com>
2020-09-13 01:46:07 -0700

    fix styletron react

--- packages/web-server/src/styletron-react/styletron-react.ts
+++ packages/web-server/src/styletron-react/styletron-react.ts
@@ -1,1 +1,6 @@
-export * from "styletron-react";
+import * as all from "styletron-react";
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const { styled, ...otherProps } = all;
+
+export default otherProps;
