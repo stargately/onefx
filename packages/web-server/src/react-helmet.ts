b8ce35b6fbed27b78424f0dfd9846038047b253e
yrjkqq <yuangu.1994@gmail.com>
2020-09-14 22:32:04 +0800

    update d.ts

--- packages/web-server/src/react-helmet.ts
+++ packages/web-server/src/react-helmet.ts
@@ -1,1 +1,1 @@
-export { default } from "react-helmet";
+export * from "react-helmet";
--- packages/web-server/src/styletron-react/styletron-react.ts
+++ packages/web-server/src/styletron-react/styletron-react.ts
@@ -3,4 +3,6 @@ import * as all from "styletron-react";
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const { styled, ...otherProps } = all;
 
+export { StyleObject } from "styletron-react";
+
 export default otherProps;
