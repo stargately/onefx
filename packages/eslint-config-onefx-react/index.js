60e6f2af44f33f5f140c501fcd6a9d6829cb1d56
Tian Pan <puncsky@gmail.com>
2020-10-12 18:44:48 -0700

    adjust eslint

--- packages/eslint-config-onefx-react/index.js
+++ packages/eslint-config-onefx-react/index.js
@@ -1,13 +1,15 @@
 module.exports = {
-  extends: ["plugin:react/recommended"],
+  extends: ["plugin:react/recommended", "prettier/react"],
+  plugins: ["graphql", "react-hooks"],
   rules: {
     "no-restricted-imports": [
       "error",
       {
         name: "antd",
-        message: "import antd/lib/* instead.",
+        message: "import antd/lib/* instead."
       }
     ],
-    "react/prop-types": 0
-  },
+    "react/prop-types": 0,
+    "react/jsx-curly-brace-presence": 2
+  }
 };
